6971b0467135ecf8b625b78917523a961a4bedcf
Claire Moore <claire.moore@marine.ie>
2021-07-07 17:07:27 +0100

    intercatch age set up

--- CelticSea/FLBEIA_model/data_02_intercatch_age.R
+++ CelticSea/FLBEIA_model/data_02_intercatch_age.R
@@ -20,25 +20,14 @@ library(ggplot2)
 
 # Merging age data ####
 # Data sources vary per stock: 
-#       - InterCatch CATON with distribution: meg.27.7b-k8abd, sol.27.7fg
-#       - InterCatch CATON without distribution: mon.27.78abd
-#       - Raised outside InterCatch: cod.27.7e-k, had.27.7b-k, whg.27.7b-ce-k, 
-#       - Supplied by NEP expert: nep.fu.16, nep.fu.17, nep.fu.19, nep.fu.2021, nep.fu.22, nep.out.7
+#       - InterCatch CANUM with distribution: meg.27.7b-k8abd, sol.27.7fg
+#       - Raised outside InterCatch: cod.27.7e-k, had.27.7b-k, whg.27.7b-ce-k, mon.27.78abd 
 
-
-# 01 - InterCatch CATON ####
-
+# 01 - InterCatch CANUM ####
 # with distributions 
-taf.unzip("bootstrap/data/ices_intercatch/2020 06 22 WGMIXFISH CATON Stocks with distributions all WG 2002  2019.zip", files="2020 06 22 WGMIXFISH CATON stocks with distributions all WG 2002 2019.csv", exdir="bootstrap/data/ices_intercatch")
+taf.unzip("bootstrap/data/ices_intercatch/2019 06 22 WGMIXFISH CANUM WECA for stocks with distributions all WG 2002 2019.zip", files="2019 06 22 WGMIXFISH CANUM WECA for stocks with distributions all WG 2002 2019.csv", exdir="bootstrap/data/ices_intercatch")
 #NB gitignore this file as it is too big
-intercatch_caton <-  read.csv(file = file.path("bootstrap/data/ices_intercatch/2020 06 22 WGMIXFISH CATON stocks with distributions all WG 2002 2019.csv"),fileEncoding = "UTF-8-BOM")
-
-# without distributions 
-taf.unzip("bootstrap/data/ices_intercatch/2020 06 22 WGMIXFISH CATON stocks withOUT distributions all WG 2002 2019.zip", files="2020 06 22 WGMIXFISH CATON stocks withOUT distributions all WG 2002 2019.csv", exdir="bootstrap/data/ices_intercatch")
-#NB gitignore this file as it is too big
-intercatch_caton_no_dist <-  read.csv(file = file.path("bootstrap/data/ices_intercatch/2020 06 22 WGMIXFISH CATON stocks withOUT distributions all WG 2002 2019.csv"),fileEncoding = "UTF-8-BOM")
-names(intercatch_caton_no_dist) <- names(intercatch_caton)
-intercatch_caton <- rbind(intercatch_caton_no_dist, intercatch_caton)
+intercatch_caton <-  read.csv(file = file.path("bootstrap/data/ices_intercatch/2019 06 22 WGMIXFISH CANUM WECA for stocks with distributions all WG 2002 2019.csv"),fileEncoding = "UTF-8-BOM")
 
 #subset for case study area
 intercatch_caton <- intercatch_caton %>% filter(Area %in% c("27.7"  , "27.7.b" , "27.7.c","27.7.c.1","27.7.c.2" , "27.7.d",  "27.7.e",  "27.7.f" , "27.7.g" , "27.7.h",  "27.7.j","27.7.j.1","27.7.j.2" , "27.7.k","27.7.k.1","27.7.k.2" ))
